8bccb3b2d965cb1e97026d7a5fbb82addeaa3fd4
Ignacio Buioli <ibuioli@gmail.com>
2020-03-02 23:55:05 -0300

    feat: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OlMapComponent } from './ol-map/ol-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlMapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ol-map/ol-map.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, AfterViewInit, Input, ElementRef } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import * as Proj from 'ol/proj';
+import { defaults as defaultControls } from 'ol/control';
+
+export const DEFAULT_HEIGHT = '500px';
+export const DEFAULT_WIDTH = '500px';
+
+@Component({
+  selector: 'ol-map',
+  templateUrl: './ol-map.component.html',
+  styleUrls: ['./ol-map.component.css']
+})
+export class OlMapComponent implements OnInit, AfterViewInit {
+  @Input() lat: number;
+  @Input() lon: number;
+  @Input() zoom: number;
+  @Input() width: string | number = DEFAULT_WIDTH;
+  @Input() height: string | number = DEFAULT_HEIGHT;
+  map: Map;
+
+  private mapEl: HTMLElement;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+    this.mapEl = this.elementRef.nativeElement.querySelector('#map');
+    this.setSize();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.width, this.height);
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: Proj.fromLonLat([this.lon, this.lat]),
+        zoom: this.zoom
+      }),
+      controls: defaultControls().extend([])
+    });
+  }
+
+  private setSize() {
+    if (this.mapEl) {
+      const styles = this.mapEl.style;
+      styles.height = coerceCssPixelValue(this.height) || DEFAULT_HEIGHT;
+      styles.width = coerceCssPixelValue(this.width) || DEFAULT_WIDTH;
+    }
+  }
+
+}
+
+const cssUnitsPattern = /([A-Za-z%]+)$/;
+
+function coerceCssPixelValue(value: any): string {
+  if (value == null) {
+    return '';
+  }
+
+  return cssUnitsPattern.test(value) ? value : `${value}px`;
+}
