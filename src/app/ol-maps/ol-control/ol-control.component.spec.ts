beaf85913ea6694904b54c689d8d5d86be1c2565
Ignacio Buioli <ibuioli@gmail.com>
2021-05-20 15:18:18 -0300

    feat: update to NG12

--- src/app/ol-maps/ol-control/ol-control.component.spec.ts
+++ src/app/ol-maps/ol-control/ol-control.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OlControlComponent } from './ol-control.component';
 
@@ -6,7 +6,7 @@ describe('OlControlComponent', () => {
   let component: OlControlComponent;
   let fixture: ComponentFixture<OlControlComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OlControlComponent ]
     })
--- src/app/ol-maps/ol-map-marker/ol-map-marker.component.spec.ts
+++ src/app/ol-maps/ol-map-marker/ol-map-marker.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OlMapMarkerComponent } from './ol-map-marker.component';
 
@@ -6,7 +6,7 @@ describe('OlMapMarkerComponent', () => {
   let component: OlMapMarkerComponent;
   let fixture: ComponentFixture<OlMapMarkerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OlMapMarkerComponent ]
     })
